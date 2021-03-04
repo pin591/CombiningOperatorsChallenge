b960cc3442be06ce0505c4c49332229d3205953a
Ana <anarebollo@xpertai.com>
2021-03-04 17:04:31 +0100

    challenge solution

--- PlaygroundCombiningOperators.playground/Contents.swift
+++ PlaygroundCombiningOperators.playground/Contents.swift
@@ -1,4 +1,24 @@
 import UIKit
 import RxSwift
 
+example(of: "zip + scan") {
+  
+  let disposeBag = DisposeBag()
+  
+  let runtimeKeyValues = Observable.from(runtimes)
+  
+  let scanTotals = runtimeKeyValues.scan(0) { runningTotal, movie in
+    runningTotal + movie.value
+  }
+  
+  let results = Observable.zip(runtimeKeyValues, scanTotals) {
+    ($0.key, $0.value, $1)
+  }
+  
+  results
+    .subscribe(onNext: {
+      print("\($0):", stringFrom($1), "(\(stringFrom($2)))")
+    })
+    .disposed(by: disposeBag)
+}
 
